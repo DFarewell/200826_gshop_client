3e851f9ebcf1e21287a7856ce4618bd2bc92e97b
Emotional昔年 <2529298454@qq.com>
2021-09-11 13:34:34 +0800

    day1

---
+++ src/pages/Search/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Search组件</h2>
+    <h3>params.keyword: {{$route.params.keyword}}</h3>
+    <h4>query.keyword2: {{$route.query.keyword2}}</h4>
+    <h4>props.keyword3: {{keyword3}}</h4>
+    <h4>props.keyword4: {{keyword4}}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  props: ['keyword3', 'keyword4']
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './routes'
+Vue.use(VueRouter)
+const originalPush = VueRouter.prototype.push
+const originalReplace = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject)
+    return originalPush.call(this, location, onResolve, onReject)
+  return originalPush.call(this, location).catch((err) => {
+    if (VueRouter.isNavigationFailure(err)) {
+      // resolve err
+      return err
+    }
+    // rethrow error
+    return Promise.reject(err)
+  })
+}
+VueRouter.prototype.Replace = function (location, onResolve, onReject) {
+    if (onResolve || onReject)
+      return originalReplace.call(this, location, onResolve, onReject)
+    return originalReplace.call(this, location).catch((err) => {
+      if (VueRouter.isNavigationFailure(err)) {
+        // resolve err
+        return err
+      }
+      // rethrow error
+      return Promise.reject(err)
+    })
+  }
+export default new VueRouter({
+    mode: 'history',
+    routes
+})
